91f9cec6eb022b145b9e42ed1f5772bdee0a6595
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2025-01-22 12:15:53 -0300

    Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export const config = {
+  tabWidth: 2,
+  arrowParens: "always",
+  semi: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
